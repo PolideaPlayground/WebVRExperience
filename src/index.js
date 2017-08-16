ad5fe4d1a1e62b38d4f2e5e5504270268e90a78b
Katarzyna Kucharczyk <katarzyna.kucharczyk@polidea.com>
2017-08-16 10:04:30 +0200

    Basic boilerplate

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import 'aframe';
+import 'aframe-particle-system-component';
+import {Entity, Scene} from 'aframe-react';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class VRScene extends React.Component {
+  render () {
+    return (
+      <Scene>
+        <Entity geometry={{primitive: 'box'}} material={{color: 'red'}} position={{x: 0, y: 0, z: -5}}/>
+        <Entity particle-system={{preset: 'snow'}}/>
+        <Entity light={{type: 'point'}}/>
+        <Entity text={{value: 'Hello, WebVR!'}}/>
+      </Scene>
+    );
+  }
+}
+
+ReactDOM.render(<VRScene/>, document.querySelector('#sceneContainer'));
