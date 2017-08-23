b7561d1f10db886f329a7e9d5f72b455a033cf2c
Chris Wróbel <krzysztof.wrobel@polidea.com>
2017-08-23 10:15:27 +0200

    Missing red cube animation

--- src/components/boxes.js
+++ src/components/boxes.js
@@ -14,7 +14,14 @@ export default class Boxes extends React.Component {
                 <ClickableBox position="-2 2 -4" color="violet"/>
                 <a-entity position="-2 4 -4">
                     <a-entity mixin="cube red" class="intersectable">
-
+                        <a-animation
+                            begin="click"
+                            attribute="position"
+                            from="0 0 0"
+                            to="0 0 -10"
+                            dur="2000"
+                            fill="both"
+                        />
                     </a-entity>
                 </a-entity>
             </Entity>
