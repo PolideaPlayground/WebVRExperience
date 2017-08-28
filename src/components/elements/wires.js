6eebc2a9cd13af19a44e9819cbfb1ddf6a93a22c
Katarzyna Kucharczyk <katarzyna.kucharczyk@polidea.com>
2017-08-28 18:23:25 +0200

    Added positions to wires

--- src/components/elements/wires.js
+++ src/components/elements/wires.js
@@ -7,34 +7,34 @@ export default class Wires extends React.Component {
                        className="menu"
                        geometry={{primitive: 'plane', width: 4, height: 4}}
                        material={{color: '#bfd7ff'}}>
-            <WireFork id="wire-fork" {...this.props}/>
-            <WireHorizontal id="wire-horuzontal" {...this.props}/>
-            <WireTurn id="wire-turn" {...this.props}/>
-            <WireVertical id="wire-vertical" {...this.props}/>
+            <WireFork id="wire-fork" />
+            <WireHorizontal id="wire-horuzontal" />
+            <WireTurn id="wire-turn"/>
+            <WireVertical id="wire-vertical" />
         </Entity>
     }
 }
 
 class WireFork extends React.Component {
     render() {
-        return <Entity {...this.props} hovered_menu_item scale={{x: 0.05, y: 0.05, z: 0.05}} position={{x: 0, y: -2, z: 0}} rotation={{x: 0, y: 0, z: 0}} ply-model="src: #wireFork"/>
+        return <Entity  hovered_menu_item scale={{x: 0.05, y: 0.05, z: 0.05}} position={{x: 0, y: -2, z: 0.5}} rotation={{x: 0, y: 0, z: 0}} ply-model="src: #wireFork"/>
     }
 }
 
 class WireHorizontal extends React.Component {
     render() {
-        return <Entity {...this.props} hovered_menu_item scale={{x: 0.05, y: 0.05, z: 0.05}} position={{x: 0, y: -1, z: 0}} rotation={{x: 0, y: 0, z: 0}} ply-model="src: #wireHorizontal"/>
+        return <Entity  hovered_menu_item scale={{x: 0.05, y: 0.05, z: 0.05}} position={{x: 0, y: -1, z: 0.5}} rotation={{x: 0, y: 0, z: 0}} ply-model="src: #wireHorizontal"/>
     }
 }
 
 class WireTurn extends React.Component {
     render() {
-        return <Entity {...this.props} hovered_menu_item scale={{x: 0.07, y: 0.05, z: 0.05}} position={{x: 1, y: -1, z: 0}} rotation={{x: 0, y: 0, z: 0}} ply-model="src: #wireTurn"/>
+        return <Entity  hovered_menu_item scale={{x: 0.07, y: 0.05, z: 0.05}} position={{x: 1, y: -1, z: 0.5}} rotation={{x: 0, y: 0, z: 0}} ply-model="src: #wireTurn"/>
     }
 }
 
 class WireVertical extends React.Component {
     render() {
-        return <Entity {...this.props} hovered_menu_item scale={{x: 0.05, y: 0.05, z: 0.05}} position={{x: 1, y: -2, z: 0}} rotation={{x: 0, y: 0, z: 0}} ply-model="src: #wireVertical"/>
+        return <Entity hovered_menu_item scale={{x: 0.05, y: 0.05, z: 0.05}} position={{x: 1, y: -2, z: 0.5}} rotation={{x: 0, y: 0, z: 0}} ply-model="src: #wireVertical"/>
     }
 }
