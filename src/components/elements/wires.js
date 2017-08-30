a7cfd75668b1e1f398a343f6fb25170e2f72666e
Katarzyna Kucharczyk <katarzyna.kucharczyk@polidea.com>
2017-08-30 16:06:15 +0200

    Changed all wires to be pickable

--- src/components/elements/wires.js
+++ src/components/elements/wires.js
@@ -9,8 +9,20 @@ export default class Wires extends React.Component {
             {
                 position: {x: -0.6, y: 0.2, z: -0.5},
                 model: "#wireFork"
+            },
+            {
+                position: {x: 0.6, y: 0.2, z: -0.5},
+                model: "#wireHorizontal"
+            },
+            {
+                position: {x: -0.6, y: -1, z: -0.5},
+                model: "#wireTurn"
+            },
+            {
+                position: {x: 0.6, y: -1, z: -0.5},
+                model: "#wireVertical"
             }
-        ]
+        ];
         this.state = {
             fields: fields
         };
@@ -22,21 +34,23 @@ export default class Wires extends React.Component {
         selectCurrentModel(el, model);
     }
 
+    createAllWires(fields) {
+        return fields.map((field) => {
+            return this.createWire(field);
+        })
+    }
+
+    createWire(field) {
+        return <Wire key={field.model} position={field.position} model={field.model}
+                     onMenuItemClicked={this.onMenuItemClicked}/>
+    }
+
     render() {
         return <Entity {...this.props}
                        className="menu"
                        geometry={{primitive: 'plane', width: 4, height: 4}}
                        material={{color: '#bfd7ff'}}>
-            {
-                this.state.fields.map((field) => {
-                    return <Wire key={field.model} position={field.position} model={field.model}
-                                 onMenuItemClicked={this.onMenuItemClicked}/>
-                })
-            }
-
-            <WireHorizontal id="wire-horizontal"/>
-            <WireTurn id="wire-turn"/>
-            <WireVertical id="wire-vertical"/>
+            {this.createAllWires(this.state.fields)}
         </Entity>
     }
 }
@@ -57,8 +71,7 @@ class Wire extends React.Component {
                                console.log('clicked')
                                this.props.onMenuItemClicked(el, this.state.model)
                            }
-                       }}
-        >
+                       }}>
             <Entity scale={{x: 0.05, y: 0.05, z: 0.05}}
                     position={this.props.position} rotation={{x: 0, y: 0, z: 0}}
                     ply-model={`src: ${this.props.model}`}
@@ -66,44 +79,3 @@ class Wire extends React.Component {
         </Entity>
     }
 }
-
-class WireFork
-    extends React
-        .Component {
-    render() {
-        return <Entity className="item intersectable" hoverable hovered_menu_item>
-            <Entity scale={{x: 0.05, y: 0.05, z: 0.05}}
-                    position={{x: -0.6, y: 0.2, z: -0.5}} rotation={{x: 0, y: 0, z: 0}} ply-model="src: #wireFork"/>
-        </Entity>
-    }
-}
-
-class WireHorizontal extends React.Component {
-    render() {
-        return <Entity className="item intersectable" hoverable hovered_menu_item>
-            <Entity scale={{x: 0.05, y: 0.05, z: 0.05}}
-                    position={{x: 0.6, y: 0.2, z: -0.5}}
-                    ply-model="src: #wireHorizontal"/>
-        </Entity>
-    }
-}
-
-class WireTurn extends React.Component {
-    render() {
-        return <Entity className="item intersectable" hoverable hovered_menu_item>
-            <Entity scale={{x: 0.05, y: 0.05, z: 0.05}}
-                    position={{x: -0.6, y: -1, z: -0.5}} ply-model="src: #wireTurn"/>
-        </Entity>
-    }
-}
-
-class WireVertical extends React.Component {
-    render() {
-        return <Entity className="item intersectable" hoverable hovered_menu_item>
-            <Entity
-                scale={{x: 0.05, y: 0.05, z: 0.05}} position={{x: 0.6, y: -1, z: -0.5}}
-                ply-model="src: #wireVertical"/>
-        </Entity>
-    }
-}
-
