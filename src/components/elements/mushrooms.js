bcc1838264ed27509325064f817a56a4a4126028
Chris Wróbel <krzysztof.wrobel@polidea.com>
2017-09-24 01:50:45 +0200

    Mushroom animation

--- src/components/elements/mushrooms.js
+++ src/components/elements/mushrooms.js
@@ -5,44 +5,82 @@ export default class Mushrooms extends React.Component {
 
     render() {
         return (
-            <Entity  {...this.props}>
+            <Entity id="mushrooms"
+                    {...this.props}>
                 <Entity
                     position={{x: 3.5, y: 0, z: -4.5}}
-                    scale={{x: 1, y: 1, z: 1}}
+                    scale="0 0 0"
                     collada-model="#mushroom"
+                    animation={{
+                        property: "scale",
+                        from: "0 0 0",
+                        to: {x: 1, y: 1, z: 1},
+                        dur: "1500",
+                        fill: "forwards",
+                        easing: "easeOutElastic",
+                        restartEvents: "grow_mushroom"
+                    }}
                 >
-                    <a-animation attribute="scale"
-                                 dur="2000"
-                                 fill="forwards"
-                                 easing="ease-out-elastic"
-                                 from="0 0 0"
-                                 to="1 1 1"
-                                 bind__visibile="visible: environment.mushroomState"
-                                 />
                 </Entity>
 
                 <Entity
                     position={{x: 5.5, y: 0.2, z: -12.5}}
-                    scale={{x: 3, y: 3, z: 3}}
+                    scale="0 0 0"
                     collada-model="#mushroom"
+                    animation={{
+                        property: "scale",
+                        from: "0 0 0",
+                        to: {x: 3, y: 3, z: 3},
+                        dur: "2000",
+                        fill: "forwards",
+                        easing: "easeOutElastic",
+                        restartEvents: "grow_mushroom"
+                    }}
                 />
 
                 <Entity
                     position={{x: 2.5, y: 0, z: -8.5}}
-                    scale={{x: 2, y: 2, z: 2}}
+                    scale="0 0 0"
                     collada-model="#mushroom"
+                    animation={{
+                        property: "scale",
+                        from: "0 0 0",
+                        to: {x: 2, y: 2, z: 2},
+                        dur: "2200",
+                        fill: "forwards",
+                        easing: "easeOutElastic",
+                        restartEvents: "grow_mushroom"
+                    }}
                 />
 
                 <Entity
                     position={{x: 11.375, y: 0.453, z: -11.8}}
-                    scale={{x: 1.5, y: 1.5, z: 1.5}}
                     collada-model="#mushroom"
+                    scale="0 0 0"
+                    animation={{
+                        property: "scale",
+                        from: "0 0 0",
+                        to: {x: 1.5, y: 1.5, z: 1.5},
+                        dur: "1500",
+                        fill: "forwards",
+                        easing: "easeOutElastic",
+                        restartEvents: "grow_mushroom"
+                    }}
                 />
 
                 <Entity
                     position={{x: 0.85, y: -0.163, z: -2.6}}
-                    scale={{x: 0.5, y: 0.5, z: 0.5}}
+                    scale="0 0 0"
                     collada-model="#mushroom"
+                    animation={{
+                        property: "scale",
+                        from: "0 0 0",
+                        to: {x: 0.5, y: 0.5, z: 0.5},
+                        dur: "1800",
+                        fill: "forwards",
+                        easing: "easeOutElastic",
+                        restartEvents: "grow_mushroom"
+                    }}
                 />
             </Entity>
         );
